bdf2845aab452fe0f8f32e9ebeda11f1da59e9cd
AlbinWukics <112380147+AlbinWukics@users.noreply.github.com>
2022-11-16 22:17:58 +0100

    Create AlbinIntro.sql

---
+++ SQLÖvningarAlbin/AlbinIntro.sql
@@ -0,0 +1,13 @@
+--CREATE TABLE KlassenDb
+--(
+--[Id] INT PRIMARY KEY,
+--[Förnamn] NVARCHAR (max) not null,
+--[Efternamn] NVARCHAR (max) not null,
+--[Ålder] INT,
+--[Hemort] NVARCHAR (max) not null
+--)
+
+--INSERT INTO KlassenDb ([Id],[Förnamn],[Efternamn],[Ålder],[Hemort])
+--VALUES (5,'Christoffer','Nordqvist',28,'Kungälv');
+
+
